0b4a76be58f9023d341720fc84ddba773cafa9f7
cgoncalveslck <claudiogoncalveslck@gmail.com>
2024-05-29 11:57:38 +0100

    try update on push the sequel

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('update test')
+  return c.text('update test but now it works')
 })
 
 export default app
